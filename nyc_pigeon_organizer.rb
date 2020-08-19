ec2b4ec23a10adb72d93ed751f5828f7b52dea37
Sean LaFlam <sean.laflam@gmail.com>
2020-08-19 04:31:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,7 +22,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, attributes_hash), final_hash|
-    attributes_hash.each do |(inner_key, names_array), |
+    attributes_hash.each do |inner_key, names_array, |
       names_array.each do |name|
         if !final_hash[name]
           final_hash[name] = {}
